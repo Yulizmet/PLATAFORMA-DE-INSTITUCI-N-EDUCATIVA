e4b74a9708e4567f034b112539f03df61ff6fe61
JairoAguilar951 <jairo.aguilar@uttn.mx>
2026-01-31 01:36:36 -0600

    Set Up Routing Mapping/Set up ViewImports for all areas

--- Program.cs
+++ Program.cs
@@ -29,6 +29,16 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+app.MapControllerRoute(
+    name: "areas",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+);
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+);
+
 
 app.MapRazorPages();
 
