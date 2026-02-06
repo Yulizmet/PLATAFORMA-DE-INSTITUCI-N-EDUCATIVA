b603d5b3ffb87d61279c8b5bf5e7ffe1ab84bb4e
Yulizmet <emilipa2004@gmail.com>
2026-02-05 18:01:36 -0600

    Add Yuli's modules

---
+++ Areas/MainScreen/Controllers/MainScreenController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SchoolManager.Areas.MainScreen.Controllers
+{
+    [Area("MainScreen")]
+    public class MainScreenController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult View2() {
+            return View();
+        }
+    }
+}
