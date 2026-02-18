5b4aebd546f7bbe749c6ec990775d3be0782bef3
Ryuran1375 <135677857+Ryuran1375@users.noreply.github.com>
2026-02-18 16:55:30 -0600

    Carga de servicio social

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PI.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Acceso (selección de dashboard)
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 🔴 CERRAR SESIÓN (SOLO REDIRIGE)
+        [HttpGet]
+        public IActionResult Logout()
+        {
+            return RedirectToAction("Index", "Account");
+        }
+    }
+}
