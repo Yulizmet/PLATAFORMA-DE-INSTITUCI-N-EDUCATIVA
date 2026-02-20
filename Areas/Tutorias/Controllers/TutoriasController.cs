192dfda7d8aca386e4a2549ce2b89f75718203db
Cristopher Freeman <cristo_freeman@hotmail.com>
2026-02-19 18:37:23 -0600

    Controlller viw

--- Areas/Tutorias/Controllers/TutoriasController.cs
+++ Areas/Tutorias/Controllers/TutoriasController.cs
@@ -1,6 +1,31 @@
-﻿namespace SchoolManager.Areas.Tutorias.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SchoolManager.Areas.Tutorias.Controllers
 {
-    public class TutoriasController
+    [Area("Gestion")]
+    ////localhost:7207/Gestion/Tutorias/Seguimiento
+    //[Route("PanelTutorias")]
+    //localhost/PanelTutorias/Seguimiento
+    public class TutoriasController : Controller
     {
+        public IActionResult Asistencia()
+        {
+            return View("~/Areas/Tutorias/Views/Asistencia.cshtml");
+        }
+
+        public IActionResult EntrevistaInicial()
+        {
+            return View("~/Areas/Tutorias/Views/EntrevistaInicial.cshtml");
+        }
+
+        public IActionResult DetalleEntrevista()
+        {
+            return View("~/Areas/Tutorias/Views/DetalleEntrevista.cshtml");
+        }
+
+        public IActionResult Seguimiento()
+        {
+            return View("~/Areas/Tutorias/Views/Seguimiento.cshtml");
+        }
     }
 }
