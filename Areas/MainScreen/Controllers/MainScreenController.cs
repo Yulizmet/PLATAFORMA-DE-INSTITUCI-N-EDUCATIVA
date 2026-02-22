b88039f66ae7813e843614d4054fdebf50026b9b
Nicolle Guerrero <nicolleguerrero204@gmail.com>
2026-02-22 14:03:54 -0600

    MainScreen as start area

--- Areas/MainScreen/Controllers/MainScreenController.cs
+++ Areas/MainScreen/Controllers/MainScreenController.cs
@@ -3,48 +3,58 @@
 namespace SchoolManager.Areas.MainScreen.Controllers
 {
     [Area("MainScreen")]
+    [Route("MainScreen/[controller]/[action]")]
     public class MainScreenController : Controller
     {
-
+        [Route("/", Order = -1)]
         public IActionResult Index()
         {
             return View();
         }
+
+        [Route("/Admin")]
         public IActionResult Admin()
         {
             return View();
         }
 
+        [Route("/Estudiantes")]
         public IActionResult Estudiantes()
         {
             return View();
         }
 
+        [Route("/Docentes")]
         public IActionResult Docentes()
         {
             return View();
         }
 
+        [Route("/SistemaEscolar")]
         public IActionResult SistemaEscolar()
         {
             return View();
         }
 
+        [Route("/ServicioSocial")]
         public IActionResult ServicioSocial()
         {
-            return RedirectToPage("/Index");
+            return View();
         }
 
+        [Route("/Extension")]
         public IActionResult Extension()
         {
             return View();
         }
 
+        [Route("/Tramites")]
         public IActionResult Tramites()
         {
             return View();
         }
 
+        [Route("/ProcesoInscripcion")]
         public IActionResult ProcesosInscripcion()
         {
             return View();
