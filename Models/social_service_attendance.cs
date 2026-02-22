faa13bfff49cecb07d4451eb462c6295bdd68299
Ryuran1375 <135677857+Ryuran1375@users.noreply.github.com>
2026-02-22 16:12:11 -0600

    Creacion de modelos de servicio social

---
+++ Models/social_service_attendance.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SchoolManager.Models
+{
+    public class social_service_attendance
+    {
+        [Key]
+        public int AttendanceId { get; set; }
+
+        [Required]
+        public int StudentId { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        [Required]
+        public bool IsPresent { get; set; }
+
+        // Indica el tipo de asistencia; por defecto "Servicio Social"
+        [Required]
+        public string Tipo { get; set; } = "Servicio Social";
+
+        public string? Notes { get; set; }
+
+        [ForeignKey("StudentId")]
+        public virtual users_user? Student { get; set; }
+    }
+}
---
+++ Models/social_service_log.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SchoolManager.Models
+{
+    public class social_service_log
+    {
+        [Key]
+        public int LogId { get; set; }
+
+        [Required]
+        public int StudentId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Week { get; set; } = null!;
+
+        [Required]
+        public string Activities { get; set; } = null!;
+
+        public int HoursPracticas { get; set; }
+
+        public int HoursServicioSocial { get; set; }
+
+        public string? Observations { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        [ForeignKey("StudentId")]
+        public virtual users_user? Student { get; set; }
+    }
+}
