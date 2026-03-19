66c25db42198b23224456943dc16f0eaebf30c99
Yulizmet <emilipa2004@gmail.com>
2026-03-19 17:58:44 -0500

    Advances

--- Areas/UserMng/Controllers/AccountContoller.cs
+++ Areas/UserMng/Controllers/AccountContoller.cs
@@ -40,7 +40,7 @@ namespace SchoolManager.Areas.UserMng.Controllers
             var usuario = _context.Users
                 .FirstOrDefault(u => u.Email == model.Email && u.IsActive == true);
 
-            if (usuario == null || !BCrypt.Net.BCrypt.Verify(model.Password, usuario.PasswordHash))
+            if (usuario == null && !BCrypt.Net.BCrypt.Verify(model.Password, usuario.PasswordHash))
             {
                 ModelState.AddModelError(string.Empty, "Correo o contraseña incorrectos.");
                 return View(model);
@@ -130,6 +130,8 @@ namespace SchoolManager.Areas.UserMng.Controllers
         
         private IActionResult RedirectByRole(ClaimsPrincipal principal)
         {
+            return RedirectToAction("Index", "MainScreen", new { area = "MainScreen" });
+
             if (principal.IsInRole("Administrator"))
             {
                 return RedirectToAction("Index", "Teachers", new { area = "MainScreen" });
